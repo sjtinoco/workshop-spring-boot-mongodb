080558755ea516c26343e140d6acdcc2b14ff85c
sjtinoco <sjtinoco@yahoo.com.br>
2024-10-07 15:04:09 -0300

    Obtendo um usuário por id

--- src/main/java/com/prospecti/workshopmongo/resources/UserResource.java
+++ src/main/java/com/prospecti/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,4 +33,9 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@GetMapping(value = "/{id}") //@RequestMapping(method=RequestMethod.GET) 
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){ //@PathVariable para "/{id}" ser reconhecido por String id
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 }
