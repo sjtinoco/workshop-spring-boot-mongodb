92b59421699f50af852d3c2e88f75cd0f4ad4b2f
sjtinoco <sjtinoco@yahoo.com.br>
2024-10-10 09:14:02 -0300

    Atualização de usuário com PUT

--- src/main/java/com/prospecti/workshopmongo/resources/UserResource.java
+++ src/main/java/com/prospecti/workshopmongo/resources/UserResource.java
@@ -56,4 +56,12 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build(); //noContent() : 204
 	}
+
+	@RequestMapping(value = "/{id}", method=RequestMethod.PUT) 
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id){
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build(); //noContent() : 204
+	}
 }
